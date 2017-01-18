59152b153e2e2dc7c9aa1882fa5bfe636bcbd096
Atsushi Igarashi <igarashi@kuis.kyoto-u.ac.jp>
2017-01-18 22:31:34 +0900

    Change to a more explicit style about function pointers.

--- src/C/hofun/hofun.c
+++ src/C/hofun/hofun.c
@@ -5,16 +5,11 @@
 
 #include <stdio.h>   // for printf
 
-int sigma(int f(int), int n) {
-  /* Typical textbooks uses notation like "int (*f)(int)" to declare a
-     parameter for function pointers.  Likewise, they use "(*f)(0)" to
-     call a function through a pointer.
-  */
-
+int sigma(int (*f)(int), int n) {
   if (n < 1) {
-    return f(0);
+    return (*f)(0);
   } else {
-    return f(n) + sigma(f, n - 1);
+    return (*f)(n) + sigma(f, n - 1);
   }
 }
 
@@ -27,8 +22,8 @@ int cube(int n) {
 }
 
 int main(void) {
-  printf("1^2 + ... + 20^2 = %d\n", sigma(square, 20));
-  printf("1^3 + ... + 20^3 = %d\n", sigma(cube, 20));
+  printf("1^2 + ... + 20^2 = %d\n", sigma(&square, 20));
+  printf("1^3 + ... + 20^3 = %d\n", sigma(&cube, 20));
   
   return 0;
 }
