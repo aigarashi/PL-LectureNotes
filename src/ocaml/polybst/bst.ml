c15571b6c943d056752cba3bce542a341f72347d
Atsushi Igarashi <igarashi@kuis.kyoto-u.ac.jp>
2017-01-19 23:12:50 +0900

    Add a comparator parameter to find, insert, and delete.

--- src/ocaml/polybst/bst.ml
+++ src/ocaml/polybst/bst.ml
@@ -10,23 +10,27 @@ type 'elm tree =
     }
 
 (* (Recursive) function find, which returns whether given integer n exists in BST t *)
-let rec find t n =
+let rec find cmp t n =
+  (* cmp is a function parameter; it takes two arguments x and y of
+     the same type and returns 0 if x is equal to y, a negative
+     integer if x is less than y, and a positive integer if x is
+     greater than y. *)
   match t with
     Lf -> false
   | Br {left=l; value=v; right=r} ->
-     if n = v then true
-     else if n < v then find l n
-     else (* n > v *) find r n
+     if cmp n v = 0 then true
+     else if cmp n v < 0 then find cmp l n
+     else (* n > v *) find cmp r n
 
 (* (Recursive) function insert, which, given BST t and a new element n, returns
    a new binary search tree with n *)
-let rec insert t n =
+let rec insert cmp t n =
   match t with
     Lf -> Br {left=Lf; value=n; right=Lf}
   | Br {left=l; value=v; right=r} ->
-     if n = v then t
-     else if n < v then Br {left=insert l n; value=v; right=r}
-     else (* n > v *) Br {left=l; value=v; right=insert r n}
+     if cmp n v = 0 then t
+     else if cmp n v < 0 then Br {left=insert cmp l n; value=v; right=r}
+     else (* n > v *) Br {left=l; value=v; right=insert cmp r n}
 
 (* Function min, which, given BST t, returns the minimum value stored in t.
    If t is empty, it returns -255. *)
@@ -40,20 +44,20 @@ let rec min t =
    be deleted, returns a new binary search tree without n.  If n is not
    stored in t, it returns t as it is. *)
 
-let rec delete t n =
+let rec delete cmp t n =
   match t with
     Lf -> t
   | Br {left=l; value=v; right=r} ->
-     if n = v then
+     if cmp n v = 0 then
        match l, r with
 	 Lf, Lf -> Lf
        | Br _, Lf -> l
        | Lf, Br _ -> r
        | Br _, Br _ ->
 	  let m = min r in
-	  Br {left=l; value=m; right=delete r m}
-     else if n < v then Br {left=delete l n; value=v; right=r}
-     else (* n > v *) Br {left=l; value=v; right=delete r n}
+	  Br {left=l; value=m; right=delete cmp r m}
+     else if cmp n v < 0 then Br {left=delete cmp l n; value=v; right=r}
+     else (* n > v *) Br {left=l; value=v; right=delete cmp r n}
 
 (* Constructing a sample tree *)
 let t1 = Br {left = Lf; value = 10; right = Lf}
@@ -64,20 +68,20 @@ let t5 = Br {left = Lf; value = 48; right = t4}
 let t6 = Br {left = t3; value = 30; right = t5}
 
 (* Testing find *)
-let test1 = find t6 30  (* should be true *)
-let test2 = find t6 13  (* should be false *)
+let test1 = find (fun x y -> x - y) t6 30  (* should be true *)
+let test2 = find (fun x y -> x - y) t6 13  (* should be false *)
 
 (* Testing insert *)
-let t7 = insert t6 23
-let t8 = insert t6 0
-let test3 = find t7 23  (* should return true *)
-let test4 = find t8 30  (* should return false *)
-let test5 = find t8 23  (* should return false *)
+let t7 = insert (fun x y -> x - y) t6 23
+let t8 = insert (fun x y -> x - y) t6 0
+let test3 = find (fun x y -> x - y) t7 23  (* should return true *)
+let test4 = find (fun x y -> x - y) t8 30  (* should return false *)
+let test5 = find (fun x y -> x - y) t8 23  (* should return false *)
 
 (* Testing delete *)
-let t9 = delete t8 30
-let test6 = find t9 30
-let test7 = find t9 48
+let t9 = delete (fun x y -> x - y) t8 30
+let test6 = find (fun x y -> x - y) t9 30
+let test7 = find (fun x y -> x - y) t9 48
 
 (* Constructing another sample tree *)
 let t11 = Br {left = Lf; value = "I"; right = Lf}
@@ -88,17 +92,17 @@ let t15 = Br {left = Lf; value = "think"; right = t14}
 let t16 = Br {left = t13; value = "so?"; right = t15}
 
 (* Testing find *)
-let test11 = find t16 "so?"  (* should be true *)
-let test12 = find t16 "Ocaml"  (* should be false *)
+let test11 = find String.compare t16 "so?"  (* should be true *)
+let test12 = find String.compare t16 "Ocaml"  (* should be false *)
 
 (* Testing insert *)
-let t17 = insert t16 "Me"
-let t18 = insert t16 "too"
-let test13 = find t17 "Me"  (* should return true *)
-let test14 = find t18 "Why"  (* should return false *)
-let test15 = find t18 "Me"  (* should return false *)
+let t17 = insert String.compare t16 "Me"
+let t18 = insert String.compare t16 "too"
+let test13 = find String.compare t17 "Me"  (* should return true *)
+let test14 = find String.compare t18 "Why"  (* should return false *)
+let test15 = find String.compare t18 "Me"  (* should return false *)
 
 (* Testing delete *)
-let t19 = delete t18 "Why"
-let test16 = find t19 "Why" (* should return false *)
-let test17 = find t19 "She" (* should return false *)
+let t19 = delete String.compare t18 "Why"
+let test16 = find String.compare t19 "Why" (* should return false *)
+let test17 = find String.compare t19 "She" (* should return false *)
