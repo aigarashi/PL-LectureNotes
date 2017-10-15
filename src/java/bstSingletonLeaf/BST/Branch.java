7287870ab19d273635214f6e9e836cbcfda408cd
igarashi <igarashi@kuis.kyoto-u.ac.jp>
2017-10-15 14:48:32 +0900

    BST with leaf being singleton and constr for branch being hidden

---
+++ src/java/bstSingletonLeaf/BST/Branch.java
@@ -0,0 +1,108 @@
+package BST;
+/**
+ * Write a description of class Branch here.
+ * 
+ * @author Atsushi Igarashi
+ * @version 20160912
+ */
+public class Branch implements BinarySearchTree {
+    // instance variables to hold a number and subtrees
+    private BinarySearchTree left;
+    private int v;  // standing for a value
+    private BinarySearchTree right;
+
+    /**
+     * Constructor for objects of class Branch
+     */
+    Branch(BinarySearchTree left, int v, BinarySearchTree right) {
+        this.left = left;
+        this.v = v;
+        this.right = right;
+    }
+
+    /**
+     * A method to ask if the tree is a leaf.
+     * 
+     * @return      whether the tree is a leaf or not
+     */
+    public boolean isLeaf() { return false; }
+
+    /**
+     * A method to ask if the tree is a branch.
+     * 
+     * @return      whether the tree is a branch or not
+     */
+    public boolean isBranch() { return true; }
+
+    /**
+     * A method to find the given number in a BST.
+     * 
+     * @param  n    the number to be searched for
+     * @return      whether n is found in the BST
+     */
+    public boolean find(int n) {
+        if (n == v) { return true; }
+        else if (n < v) { return left.find(n); }
+        else /* n > v */ { return right.find(n); }
+    }
+
+    /**
+     * A method to insert a number into a BST.
+     * 
+     * @param  n    the number to be added
+     * @return      a new BST with n (or the same tree, if n is already in the BST)
+     */
+    public BinarySearchTree insert(int n) {
+        if (n == v) {
+            return this;
+        } else if (n < v) {
+            BinarySearchTree newLeft = left.insert(n);
+            return new Branch(newLeft, v, right);
+        } else /* n > v */ {
+            BinarySearchTree newRight = right.insert(n);
+            return new Branch(left, v, newRight);
+        }
+    }
+
+    /**
+     * A method to find a minimum number in a BST
+     * 
+     * @return      a minimum number in the BST (or -255, if the tree is empty)
+     */
+    public int min() {
+        if (left.isLeaf()) { return v; }
+        else { return left.min(); }
+    }
+
+    /**
+     * A method to delete a number from a BST.
+     * 
+     * @param  n    the number to be deleted
+     * @return      a new BST without n (or the same tree, if n is not in the BST)
+     */
+    public BinarySearchTree delete(int n) {
+        if (n == v) {
+            if (left.isLeaf()) {
+                if (right.isLeaf()) {
+                    return Leaf.obj;
+                } else {
+                    return right;
+                }
+            } else {
+                if (right.isLeaf()) {
+                    return left;
+                } else {
+                    int m = right.min();
+                    BinarySearchTree newRight = right.delete(m);
+                    return new Branch(left, m, newRight);
+                }
+            }
+        } else if (n < v) {
+            BinarySearchTree newLeft = left.delete(n);
+            return new Branch(newLeft, v, right);
+        } else /* n > v */ {
+            BinarySearchTree newRight = right.delete(n);
+            return new Branch(left, v, newRight);
+        }
+    }
+}
---
+++ src/java/bstSingletonLeaf/Main.java
@@ -0,0 +1,34 @@
+/**
+ * Write a description of class Main here.
+ * 
+ * @author Atsushi Igarashi
+ * @version 20160824
+ */
+import BST.*;
+public class Main {
+    public static void main(String[] args) {
+        BinarySearchTree t1 = Leaf.obj.insert(30);
+        BinarySearchTree t2 = t1.insert(15);
+        BinarySearchTree t3 = t2.insert(10);
+        BinarySearchTree t4 = t3.insert(25);
+        BinarySearchTree t5 = t4.insert(48);
+        BinarySearchTree t6 = t5.insert(60);
+        boolean test1 = t6.find(30);  // should be true
+        boolean test2 = t6.find(13);  // should be false
+        BinarySearchTree t7 = t6.insert(23);
+        BinarySearchTree t8 = t6.insert(0);
+        boolean test3 = t7.find(23);  // should be true
+        boolean test4 = t8.find(30);  // should be true
+        boolean test5 = t8.find(23);  // should be false
+        BinarySearchTree t9 = t8.delete(30);
+        boolean test6 = t9.find(30);  // should be false
+        boolean test7 = t9.find(48);  // should be true
+        System.out.println("test 1: " + test1);
+        System.out.println("test 2: " + test2);
+        System.out.println("test 3: " + test3);
+        System.out.println("test 4: " + test4);
+        System.out.println("test 5: " + test5);
+        System.out.println("test 6: " + test6);
+        System.out.println("test 7: " + test7);
+    }
+}
